cc56d4584778d45f886240824aae1af011839edc
Gaeun Lee <67837142+gaeun0506@users.noreply.github.com>
2023-06-07 16:08:08 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 31732 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -16,26 +16,24 @@ def dfs(x,y):
     current_color = matrix[x][y]
 
     for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
+        nx, ny = x + dx[k], y + dy[k]
         if (0 <= nx < n) and (0 <= ny < n):
             #현재 좌표의 색상과 상하좌우 좌표에 있는 색상이 같으면 dfs로 넣어준다.
-            if visited[nx][ny]==False:
-                if matrix[nx][ny] == current_color:
+            if visited[nx][ny]==False and matrix[nx][ny] == current_color:
                     dfs(nx,ny)
 
 for i in range(n):
     for j in range(n):
         # 방문하지 않은 좌표이면 dfs로 넣어준다.
-        if visited[i][j]==False:
+        if visited[i][j] == False:
             dfs(i,j)
             three_cnt += 1
 
 #R을 G로 바꾸어준다. --> 적록색약은 같은 색으로 보기 때문에
 for i in range(n):
     for j in range(n):
-        if matrix[i][j]=='R':
-            matrix[i][j]='G'
+        if matrix[i][j] == 'R':
+            matrix[i][j] = 'G'
 
 visited = [[False] * n for _ in range(n)]
 
